7f6598b213a472ee1bc322e277054e323a9ba3fb
Abel Aron Amieva <abelamieva@gmail.com>
2024-03-10 05:13:45 -0300

    colores globales establecidos

--- src/presentation/theme/theme.ts
+++ src/presentation/theme/theme.ts
@@ -1,7 +1,14 @@
 import { StyleSheet } from "react-native";
 
 export const globalColors = {
-
+    primary: '#7037eb',
+    secondary: '#f72585',
+    tertiary: '#3a0ca3',
+    success: '#4cc9f0',
+    warning: '#fca311',
+    danger: '#e71d36',
+    dark: '#22223b',
+    background: '#fff',
 };
 
 export const globalStyle = StyleSheet.create({
